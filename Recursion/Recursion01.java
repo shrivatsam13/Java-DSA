c498780613b91340335b8deed75d2cbda4f9a911
shrivats13 <shrivatsam43@gmail.com>
2024-03-18 18:58:11 +0530

    Changes in recursion of fibonacci

--- Recursion/Recursion01.java
+++ Recursion/Recursion01.java
@@ -6,15 +6,15 @@ public class Recursion01 {
 
     static int factorial = 1;
     static int sum = 0;
-    static int num = 0;
+    static int num;
     static int[] array;
 
-    public static int[] fibonacciSeries(int a, int b, int n) {
+    public static void fibonacciSeries(int a, int b, int n) {
         if (n > 1) {
             fibonacciSeries(b, a+b, n-1);
         }
         array[n-1] = a;
-        return array;
+//        array[n-1] = a+b;
     }
 
     public static int factorial(int n) {
@@ -41,13 +41,13 @@ public class Recursion01 {
         num = sc.nextInt();
         array = new int[num];
 
-        System.out.println("\nFactorial of '"+num+"' is: "+factorial(num));
+        System.out.println("Factorial of '"+num+"' is: "+factorial(num));
 
         System.out.println("\nSum of first '"+num+"' natural numbers is: "+sumOfNNumbers(num));
 
-        int[] array = fibonacciSeries(0, 1, num);
+        fibonacciSeries(0, 1, num);
         System.out.print("\nFibonacci series of '"+num+"' numbers is: ");
-        for (int i = num-1; i >= 0 ; i--) {
+        for (int i = array.length-1; i >= 0 ; i--) {
             System.out.print(array[i]+" ");
         }
     }
---
+++ Recursion/Recursion02.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class Recursion02 {
+
+    //Tower of Hanoi
+
+    public static void main(String[] args) {
+
+    }
+}
